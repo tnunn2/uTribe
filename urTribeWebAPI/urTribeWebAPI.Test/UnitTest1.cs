fb1a9aa2c4847c35c1305fcece31b313f7f564ca
Thomas Nunnery <tnunn2@gmail.com>
2015-02-09 00:43:58 -0600

    Added test project and build script

---
+++ urTribeWebAPI/urTribeWebAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace urTribeWebAPI.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(14, 7 + 7);
+        }
+    }
+}
