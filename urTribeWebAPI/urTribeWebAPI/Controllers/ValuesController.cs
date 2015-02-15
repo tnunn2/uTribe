41cdbc2e53685a94486bdec452b43fb8d36a5987
tnunn2 <tnunn2@gmail.com>
2015-02-15 09:38:59 -0600

    Update ValuesController.cs

--- urTribeWebAPI/urTribeWebAPI/Controllers/ValuesController.cs
+++ urTribeWebAPI/urTribeWebAPI/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace urTribeWebAPI.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "valueTest1";
+            return "valueTest10";
         }
 
         // POST api/values
