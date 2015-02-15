9e60dfe45d8b249e959a1a39552107eadcf8a455
Thomas Nunnery <tnunn2@gmail.com>
2015-02-15 15:33:02 -0600

    Adding mocking framework to unit test

---
+++ urTribeWebAPI/urTribeWebAPI/TestCustomerService.cs
@@ -0,0 +1,25 @@
+﻿public class CustomerService
+{
+    private ICustomerRepository _repository;
+
+    public CustomerService(ICustomerRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Customer GetCustomer(int id)
+    {
+        return _repository.GetCustomer(id);
+    }
+}
+
+public interface ICustomerRepository
+{
+    Customer GetCustomer(int id);
+}
+
+public class Customer
+{
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+}
