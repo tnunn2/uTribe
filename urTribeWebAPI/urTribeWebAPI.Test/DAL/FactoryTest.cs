e183a5f994f63822675a6734344185581b58dfd0
MSussex <contact@sussexfamily.com>
2015-02-28 14:45:06 -0600

    Add RepositoryMap File and add a simple Unit Test of the factory

---
+++ urTribeWebAPI/urTribeWebAPI.Test/DAL/FactoryTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using urTribeWebAPI.Common;
+using urTribeWebAPI.DAL.Interfaces;
+using urTribeWebAPI.DAL.Factory;
+using urTribeWebAPI.DAL.Repositories;
+using NUnit.Framework;
+using Moq;
+
+namespace urTribeWebAPI.Test.DAL
+{
+    [TestFixture]
+    class FactoryTest
+    {
+        [Test]
+        public void RetrieveRepository()
+        {
+            var factory = RepositoryFactory.Instance;
+            IUserRepository userRepository = factory.Create<IUserRepository>();
+            Assert.IsTrue(userRepository != null);
+        }           
+    }
+}
