d43ca9c4d71ea0ae751df4799f73432241277282
Thomas Nunnery <tnunn2@gmail.com>
2015-02-28 21:28:42 -0600

    Created EventsController

---
+++ urTribeWebAPI/urTribeWebAPI/Controllers/EventsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace urTribeWebAPI.Controllers
+{
+    public class EventsController : ApiController
+    {
+        // GET: api/Events
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Events/5
+        public string GetCreate(int id)
+        {
+            return "value = " + id;
+        }
+
+        // POST: api/Events
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Events/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Events/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
